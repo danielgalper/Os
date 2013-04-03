7f0959647cd2acf7d3b6071f81a13a06d3d350ce
Anton Kovsharov <antonkovsharov@gmail.com>
2013-04-03 13:24:20 +0400

    Added structure of main while

--- dupme/main.c
+++ dupme/main.c
@@ -13,5 +13,27 @@ int string_to_int(char *s) {
 
 int main(int argc, char* argv[]) {
    int k = string_to_int(argv[1]);
-   printf("%d\n", k);
+   int PRINT = 0, NO_PRINT = 1;
+   int state = PRINT;
+   char *buffer = malloc(k + 1);
+   int len = 0;
+
+   while (1) {
+       int read_result = read(0, buffer, k + 1 - len);
+       if (read_result < 0) return 1;
+
+       if (read_result > 0) {
+           int i = len;
+           for (; i < len + read_result; i++) {
+               if (buffer[i] == '\n') {
+                   buffer[i] = 0;
+                   if (state == PRINT) {
+                     write();
+                     write();
+                   } else if (state == NO_PRINT) {
+                   }
+               }
+           }
+       }
+   }  
 }
