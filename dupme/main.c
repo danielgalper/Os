6f8d39ce5e8c471ec450e307c8a2b6556472d704
Anton Kovsharov <antonkovsharov@gmail.com>
2013-04-03 14:34:19 +0400

    It seems that it works

--- dupme/main.c
+++ dupme/main.c
@@ -11,7 +11,9 @@ int string_to_int(char *s) {
     return res;
 }
 
-void write(int count) {
+char *buffer;
+
+void print_string(int count) {
     int was_writed = 0;
     while (was_writed < count) {
         was_writed += write(1, buffer + was_writed, count - was_writed);    
@@ -22,23 +24,39 @@ int main(int argc, char* argv[]) {
    int k = string_to_int(argv[1]);
    int PRINT = 0, NO_PRINT = 1;
    int state = PRINT;
-   char *buffer = malloc(k + 1);
    int len = 0;
+   
+   buffer = malloc(k + 1);
 
    while (1) {
-       int read_result = read(0, buffer, k + 1 - len);
+       int read_result = read(0, buffer + len, k + 1 - len);
        if (read_result < 0) return 1;
 
+       if (read_result == 0) {
+           break;
+       }
+       
        if (read_result > 0) {
-           int i = len;
-           for (; i < len + read_result; i++) {
-               if (buffer[i] == '\n') {
+           read_result += len;
+           for (; len < read_result;) {
+               if (buffer[len] == '\n') {
                    if (state == PRINT) {
-                     write(i + 1);
-                     write(i + 1);
+                       print_string(len + 1);
+                       print_string(len + 1);
                    } else if (state == NO_PRINT) {
+                       state = PRINT;
                    }
+                   memmove(buffer, buffer + len + 1, read_result - len - 1);
+                   read_result = read_result - len - 1;
+                   len = 0;
+               } else {
+                   len++;
                }
+           }
+
+           if (len == k + 1) {
+               len = 0;
+               state = NO_PRINT;
            }
        }
    }  
